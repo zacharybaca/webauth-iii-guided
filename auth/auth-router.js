9142ff9fef176648e6a4ddfbd21e7fe09d9dce04
zacharybaca <baca.zach@icloud.com>
2019-12-18 11:46:33 -0600

    initial

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,12 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        //sign token
+        const token = signToken(user);
+        //send the token
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          token,
+          message: `Welcome ${user.username}!`
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -37,4 +42,16 @@ router.post('/login', (req, res) => {
     });
 });
 
+function signToken(user) {
+  const payload = {
+    username: user.username
+  };
+
+  const secret = process.env.JWT_SECRET || 'keep secret';
+
+  const options = {
+    expiresIn: '1h'
+  };
+  return jwt.sign(payload, secret, options);
+}
 module.exports = router;
